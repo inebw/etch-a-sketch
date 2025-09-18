c06075d49fdece2ab54e523668ac0804a99ce698
inebw <punit965@gmail.com>
2025-09-19 00:58:10 +0530

    Displays 16 x 16 grid
    - Added a container div in html.
    - In script file added nested loop to create
    16 rows and 16 columns inside those
    - Added flex to the main container with
    flex-direction column
    - Added flex to the row with flex direction: row
    - Added black border to the col 'divs' to make
    them visible

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert('hi')
+const container = document.querySelector('.container');
+for (i = 0; i <16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row')
+    for (j = 0; j<16; j++) {
+        const col = document.createElement('div');
+        col.classList.add('col')
+        row.appendChild(col);
+    }
+    container.appendChild(row);
+}
